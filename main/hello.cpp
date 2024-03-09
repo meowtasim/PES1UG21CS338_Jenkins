7c8487f5fd6a18ac82ddb4bba24faa34141ca816
Mutasim <95741325+meowtasim@users.noreply.github.com>
2024-03-09 13:14:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Hello this is pes1ug21cs338";
+  cout << "Hello this is pes1ug21cs338 :)";
   return 0;
 }
